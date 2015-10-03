e2a52640bfca00f62ee78046de4dd81a4c81bac8
Michael Liang <michaelpliang@gmail.com>
2015-10-03 00:00:01 -0700

    updated some stuff

--- frontend/src/controllers/manage.js
+++ frontend/src/controllers/manage.js
@@ -10,6 +10,8 @@ app.controller('manageCtrl', function ($scope, $http, $state) {
     })
 
   $scope.addItem = function (item) {
+    item['owner'] = 'mike';
+    item['username'] ='mikeker3';
     console.log(item);
     $http.post('http://localhost:3000/item', item)
       .then(function (data) {
@@ -48,7 +50,5 @@ app.controller('manageCtrl', function ($scope, $http, $state) {
       console.log(error);
     });
   }
-
-
 
 });
